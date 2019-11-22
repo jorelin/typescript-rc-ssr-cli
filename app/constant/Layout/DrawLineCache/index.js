d3cf02f3761bb6044a24f50e3e4ba6abdb3f78ae
张豹 <zhangbao@hiynn.com>
2019-11-22 15:00:01 +0800

    zb-移除掉冗余的代码

--- app/constant/Layout/DrawLineCache/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// 开启起始端口线层信息
-exports.START_SOURCE_PORT_LINE_SUCCESS = "START_SOURCE_PORT_LINE_SUCCESS";
-// 重置起始端口线信息
-exports.RESET_SOURCE_PORT_LINE_SUCCESS = "RESET_SOURCE_PORT_LINE_SUCCESS";
-//# sourceMappingURL=index.js.map
--- app/containers/Layout/LayoutSider/SiderBody/Operator/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const React = require("react");
-require("./style.scss");
-function default_1() {
-    return (React.createElement("div", null, "operator"));
-}
-exports.default = default_1;
-//# sourceMappingURL=index.js.map
