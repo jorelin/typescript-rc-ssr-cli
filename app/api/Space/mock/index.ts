ddedfa630a8b1e73679e6e4d50995b39aa57f198
张豹 <zhangbao@hiynn.com>
2019-12-11 17:44:35 +0800

    zb-完善

--- app/api/Space/mock/index.ts
+++ app/api/Space/mock/index.ts
@@ -3,7 +3,7 @@
  * @Date: 2019-12-03 14:04:08
  * @Email: lovewinders@163.com
  * @Last Modified by: zhangb
- * @Last Modified time: 2019-12-11 17:13:08
+ * @Last Modified time: 2019-12-11 17:43:43
  * @Description:
  */
 import Mock from 'mockjs';
@@ -24,7 +24,7 @@ const Api: ApiProps = {
             'msg': 'success',
             // 属性 list 的值是一个数组，其中含有 1 到 10 个元素
             'result|6-20': [{
-                'id|1-100': 1,
+                'id|+1': 1,
                 'name': '@ctitle(3, 5)',
                 'remark': '标签1',
                 'admin': '@cname'
--- mock/api/space.controller.js
+++ mock/api/space.controller.js
@@ -3,7 +3,7 @@
  * @Date: 2019-12-10 16:11:58
  * @Email: lovewinders@163.com
  * @Last Modified by: zhangb
- * @Last Modified time: 2019-12-11 17:12:43
+ * @Last Modified time: 2019-12-11 17:44:10
  * @Description: 
  */
 import Mock from 'mockjs';
@@ -19,7 +19,7 @@ class SpaceController {
             'msg': 'success',
             // 属性 list 的值是一个数组，其中含有 1 到 10 个元素
             'result|6-20': [{
-                'id|1-100': 1,
+                'id|+1': 1,
                 'name': '@ctitle(3, 5)',
                 'remark': '标签1',
                 'admin': '@cname'
